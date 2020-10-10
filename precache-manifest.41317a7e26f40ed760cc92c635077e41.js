b9492d9325795975dd96645e3c6e510dac59d2f4
ofirm109 <ofirm109@walla.com>
2020-10-11 00:11:15 +0300

    Add files via upload

---
+++ precache-manifest.41317a7e26f40ed760cc92c635077e41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77be77f6583445fb0def678ef282b6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef7daf0ae3479b93f0e6",
+    "url": "/static/css/main.30d576cd.chunk.css"
+  },
+  {
+    "revision": "9711b1d14bdbde4cc547",
+    "url": "/static/js/2.d561ae1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d561ae1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7daf0ae3479b93f0e6",
+    "url": "/static/js/main.58936dfb.chunk.js"
+  },
+  {
+    "revision": "275904135d5ab8926ef9",
+    "url": "/static/js/runtime-main.b8e37697.js"
+  }
+]);
